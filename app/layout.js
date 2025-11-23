d4a386e5ff6e1457460c16b62534aba57fc3ef72
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-23 17:02:13 +0200

    added header and nav from starter and some layout classes

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,29 @@
-import Logo from "./_components/Logo.js";
-import Navigation from "./_components/Navigation.js";
-import "./_styles/globals.css"
+import Header from "./_components/Header.js";
+import "./_styles/globals.css";
 
-import {Josefin_Sans} from "next/font/google"
+import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({
-	subsets: ['latin'],
-	display: "swap"
-})
-
+	subsets: ["latin"],
+	display: "swap",
+});
 
 export const metadata = {
 	title: {
 		template: "%s / The Wild Oasis",
-		default: "Welcome / The Wild Oasis"
+		default: "Welcome / The Wild Oasis",
 	},
-	description: "Experience wilderness luxury in our exclusive mountain cabins, where nature and comfort unite"
+	description: "Experience wilderness luxury in our exclusive mountain cabins, where nature and comfort unite",
 };
 
 function RootLayout({ children }) {
 	return (
 		<html>
-			<body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-				<header>
-					<Logo />
-					<Navigation />
-				</header>
-				<main>{children}</main>
-				<footer>Copyright by hota</footer>
+			<body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}>
+				<Header />
+				<div className="flex-1 px-8 py-12">
+					<main className="max-w-7xl mx-auto">{children}</main>
+				</div>
 			</body>
 		</html>
 	);
