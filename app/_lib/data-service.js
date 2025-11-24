a8567ea1efbdce15bcab4f74e346ecdee85bbeb7
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-24 18:59:03 +0200

    added notfound page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase.js";
+import { notFound } from "next/navigation.js";
 
 /////////////
 // GET
@@ -12,6 +13,7 @@ export async function getCabin(id) {
 
 	if (error) {
 		console.error(error);
+		notFound()
 	}
 
 	return data;
