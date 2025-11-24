a4296ce41b0315a04f624804710ed8b77fa0b520
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-24 18:42:18 +0200

    added dynamic page title

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,12 @@ import { getCabin } from "@/app/_lib/data-service.js";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image.js";
 
+export async function generateMetadata({params}){
+	const { cabinId } = await params;
+	const {name} = await getCabin(cabinId)
+	return {title: `Cabin ${name}`}
+}
+
 export default async function Page({ params }) {
 	const { cabinId } = await params;
 	const cabin = await getCabin(cabinId);
