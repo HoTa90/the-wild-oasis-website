89ac4b98063384beeabeb6015018b9a93884af3f
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-24 17:52:32 +0200

    added supbase and fixed scroll bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+ 
+  images: {
+    remotePatterns: [new URL('https://tqhtjuzacanlvgwhnwlb.supabase.co/**')],
+  },
+
 };
 
 export default nextConfig;
