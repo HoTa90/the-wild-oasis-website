23363732923bdd5c13c5259f10fd6fb92ac17cb5
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-25 19:04:35 +0200

    added highlighting on selected filter

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -2,39 +2,38 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation.js";
 
+const filters = [
+	{ key: "all", label: "All" },
+	{ key: "small", label: "1—3 guests" },
+	{ key: "medium", label: "4—7 guests" },
+	{ key: "large", label: "8—12 guests" },
+];
+
 export default function Filter() {
 	const searchParams = useSearchParams();
 	const router = useRouter();
 	const path = usePathname();
 
+	const activeFilter = searchParams.get("capacity") ?? "all";
+
 	const handleFilter = (filter) => {
 		const params = new URLSearchParams(searchParams);
-		params.set("capacity", filter)
-		router.replace(`${path}?${params.toString()}`, {scroll: false})
+		params.set("capacity", filter);
+		router.replace(`${path}?${params.toString()}`, { scroll: false });
 	};
 
 	return (
-		<div className=" border border-primary-800 flex">
-			<button
-				onClick={() => handleFilter("all")}
-				className="px-5 py-2 hover:bg-primary-700">
-				All
-			</button>
-			<button
-				onClick={() => handleFilter("small")}
-				className="px-5 py-2 hover:bg-primary-700">
-				1&mdash;3 guests
-			</button>
-			<button
-				onClick={() => handleFilter("medium")}
-				className="px-5 py-2 hover:bg-primary-700">
-				4&mdash;7 guests
-			</button>
-			<button
-				onClick={() => handleFilter("large")}
-				className="px-5 py-2 hover:bg-primary-700">
-				8&mdash;12 guests
-			</button>
+		<div className="border border-primary-800 flex">
+			{filters.map((f) => (
+				<button
+					key={f.key}
+					onClick={() => handleFilter(f.key)}
+					className={`px-5 py-2 hover:bg-primary-700 text-primary-200 ${
+						activeFilter === f.key ? "bg-primary-700 " : ""
+					}`}>
+					{f.label}
+				</button>
+			))}
 		</div>
 	);
 }
