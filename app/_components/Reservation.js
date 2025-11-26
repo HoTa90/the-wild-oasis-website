76da13a7436b040d7d03bf9c071e2f76f2ea9c68
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-26 20:33:57 +0200

    added user to the reservation and loginmsg if no user

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,9 +1,12 @@
+import { auth } from "../_lib/auth.js";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service.js";
 import DateSelector from "./DateSelector.js";
+import LoginMessage from "./LoginMessage.js";
 import ReservationForm from "./ReservationForm.js";
 
 export default async function Reservation({ cabin }) {
 	const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
+	const session = await auth();
 
 	return (
 		<div className="grid grid-cols-1 lg:grid-cols-2 border border-primary-800 mt-8">
@@ -13,7 +16,7 @@ export default async function Reservation({ cabin }) {
 				bookedDates={bookedDates}
 			/>
 
-			<ReservationForm cabin={cabin} />
+			{session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
 		</div>
 	);
 }
