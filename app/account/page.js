cbd1bff85661d4ffae43596999a4c5d756b0f3a5
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-11-28 17:03:49 +0200

    added loginpage with google

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,15 @@
+import { auth } from "../_lib/auth.js";
+
 export const metadata = {
 	title: "Guest area",
 };
 
-function Page() {
+async function Page() {
+	const data = await auth()
+	console.log(data)
   return (
 	  <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-		Welcome, *USERNAME*
+		Welcome, {data?.user?.name}
       </h2>
   )
 }
